efc1df0368339fe3747a106ac17c7e507189c9d3
jgroot1 <jorritgroot@gmail.com>
2026-07-31 00:45:46 +0200

    change
    Signed-off-by: jgroot1 <jorritgroot@gmail.com>

---
+++ functions/remove_error.py
@@ -0,0 +1,19 @@
+def remove_invalid():
+
+    remove_errors = None
+    while remove_errors is None:
+        try:
+            remove_errors = input("\nDo you want to automatically remove invalid characters from the DNA?: (y/n): ").lower()
+            if remove_errors in ["y", "yes"]:
+                print("Automatically removing invalid characters from the DNA")
+                remove_errors = True
+            elif remove_errors in ["n", "no"]:
+                print("Keeping invalid characters in DNA, This can cause errors!\n")
+                remove_errors = False
+            else:
+                remove_errors = None
+                raise ValueError
+        except ValueError:
+            print("Please enter either 'y' or 'n' for yes or no.")
+
+    return remove_errors
