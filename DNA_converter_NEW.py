d02674e23c359e3b0f994288eb92edf5e25e01bb
jgroot1 <jorritgroot@gmail.com>
2026-08-02 11:35:54 +0200

    no erros at this point

--- DNA_converter_NEW.py
+++ DNA_converter_NEW.py
@@ -1,7 +1,7 @@
 # ask the user if they want to import their DNA from a file or the consol
-from utils.file_input import file_input
+from utils.input_type_chose import file_input
 input_from_file = file_input()
 
 # ask the user if they want the program to automatically remove invalid characters from the DNA
-from utils.remove_error import remove_invalid_verify_input
-remove_errors = remove_invalid_verify_input()
+from utils.remove_error_chose import remove_invalid
+remove_errors = remove_invalid()
